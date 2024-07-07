d22178a620d64d8d3c45b02fc6cd7126fa3c3f43
Lowsky <30263706+lowsky541@users.noreply.github.com>
2024-07-07 07:16:02 +0200

    Fix DisplayBoard

--- display.go
+++ display.go
@@ -5,7 +5,10 @@ import "fmt"
 func DisplayBoard(board [9][9]int) {
 	for y := 0; y < 9; y++ {
 		for x := 0; x < 9; x++ {
-			fmt.Printf("%c ", board[y][x]+'0')
+			fmt.Printf("%c", board[y][x]+'0')
+			if x != 8 {
+				fmt.Print(" ")
+			}
 		}
 		fmt.Println()
 	}
